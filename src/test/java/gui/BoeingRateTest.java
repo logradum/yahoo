c368d2119f7dce1fba2edfc1642fd88591bc2f17
Daniel <logradum@gmail.com>
2019-07-12 11:05:10 +0300

    some new - webdriver

--- src/test/java/gui/BoeingRateTest.java
+++ src/test/java/gui/BoeingRateTest.java
@@ -1,4 +1,15 @@
 package gui;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.Set;
+
 public class BoeingRateTest {
+
+
 }
+
+
